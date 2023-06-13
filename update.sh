eafea8a8022649e55bae41e0edfc5c2a3a223347
seanlandsman <sean@thelandsmans.com>
2023-06-13 21:07:27 +0100

    Updates

---
+++ update.sh
@@ -0,0 +1,65 @@
+cd /Users/seanlandsman/dev/ag-grid/latest/charts-community-modules/ag-charts-community
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/charts-enterprise-modules/ag-charts-enterprise
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/client-side-row-model
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/core
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/angular/dist/ag-grid-angular
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/styles
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-enterprise-modules/status-bar
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/react
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/vue
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-community-modules/vue3
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-enterprise-modules/core
+npm pack
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-packages/ag-grid-community
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-packages/ag-grid-enterprise
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-packages/ag-grid-react
+npm pack
+cd -
+cd /Users/seanlandsman/dev/ag-grid/latest/grid-packages/ag-grid-angular
+npm pack
+cd -
+
+cd modules/angular/node_modules
+rm -rf @ag-grid-community @ag-grid-enterprise ag-grid-community ag-grid-enterprise
+cd -
+cd modules/react/node_modules
+rm -rf @ag-grid-community @ag-grid-enterprise ag-grid-community ag-grid-enterprise
+cd -
+cd modules/webpack-ts/node_modules
+rm -rf @ag-grid-community @ag-grid-enterprise ag-grid-community ag-grid-enterprise
+cd -
+cd packages/angular/node_modules
+rm -rf @ag-grid-community @ag-grid-enterprise ag-grid-community ag-grid-enterprise
+cd -
+cd packages/react/node_modules
+rm -rf @ag-grid-community @ag-grid-enterprise ag-grid-community ag-grid-enterprise
+cd -
+cd packages/webpack-ts/node_modules
+rm -rf @ag-grid-community @ag-grid-enterprise ag-grid-community ag-grid-enterprise
+cd -
+
+npx lerna bootstrap
