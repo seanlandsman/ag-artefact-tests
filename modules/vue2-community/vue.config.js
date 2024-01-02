04b68967bf225488f117352b1850d8b7358c7e32
seanlandsman <sean@thelandsmans.com>
2024-01-02 17:58:55 +0000

    Updates

---
+++ modules/vue2-community/vue.config.js
@@ -0,0 +1,5 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/modules/vue2-community/dist',
+})
---
+++ modules/vue2-enterprise/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- The AG Grid component -->
+  <ag-grid-vue
+      style="height: 500px"
+      class="ag-theme-quartz"
+      :rowData="rowData"
+      :columnDefs="colDefs"
+  >
+  </ag-grid-vue>
+</template>
+
+<script>
+import { ref } from 'vue';
+import "@ag-grid-community/styles/ag-grid.css"; // Core CSS
+import "@ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+import {ClientSideRowModelModule} from "@ag-grid-community/client-side-row-model";
+import {StatusBarModule} from "@ag-grid-enterprise/status-bar";
+import {ModuleRegistry} from '@ag-grid-community/core';
+import { AgGridVue } from "@ag-grid-community/vue"; // Vue Grid Logic
+
+ModuleRegistry.register(ClientSideRowModelModule);
+ModuleRegistry.register(StatusBarModule);
+
+export default {
+  name: "App",
+  components: {
+    AgGridVue,
+  },
+  setup() {
+
+    // Row Data: The data to be displayed.
+    const rowData = ref([
+      {make: 'Toyota', model: 'Celica', price: 35000},
+      {make: 'Ford', model: 'Mondeo', price: 32000},
+      {make: 'Porsche', model: 'Boxster', price: 72000}
+    ]);
+
+    // Column Definitions: Defines & controls grid columns.
+    const colDefs = ref([
+      {field: 'make'},
+      {field: 'model'},
+      {field: 'price'}
+    ]);
+
+    const statusBar = ref({
+      statusPanels: [
+        {
+          statusPanel: 'agTotalAndFilteredRowCountComponent',
+          align: 'left',
+        }
+      ]
+    });
+
+    return {
+      rowData,
+      colDefs,
+      statusBar
+    };
+  },
+};
+</script>
